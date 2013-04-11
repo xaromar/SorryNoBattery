85a72fa59e2f13e23b26456a46a677ed9b91fef4
Xavi <Xavi@Sherkan.local>
2013-04-11 23:13:51 +0800

    Initial Commit

---
+++ SorryNoBattery/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SorryNoBattery
+//
+//  Created by Xavi on 11/4/13.
+//  Copyright (c) 2013 Xavier Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
